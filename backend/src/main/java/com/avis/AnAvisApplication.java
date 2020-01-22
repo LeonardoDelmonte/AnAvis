f1444c125a7456c9ad297f21d2df049bb96e9db6
Marco <marco.scarpetta@studenti.unicam.it>
2020-01-22 20:18:42 +0100

    Aggiunto Logger

--- backend/src/main/java/com/avis/AnAvisApplication.java
+++ backend/src/main/java/com/avis/AnAvisApplication.java
@@ -1,12 +1,16 @@
 package com.avis;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AnAvisApplication {
-	
+	private static final Logger logger = LoggerFactory.getLogger(AnAvisApplication.class);
 	public static void main(String[] args) {
+		
+		logger.warn("pluto");
 		SpringApplication.run(AnAvisApplication.class, args);
 	}
 
